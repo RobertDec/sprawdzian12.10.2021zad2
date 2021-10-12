b40d67b471c679ccf7bf19d5ce1cf4e9587484ac
Robert <87870041+RobertDec@users.noreply.github.com>
2021-10-12 14:47:32 +0200

    Add files via upload

---
+++ przetwarzanie plikow 2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	fstream plik("hasla.txt", ios::in);
+	fstream has("passwords.txt", ios::out);
+	string haslo;
+	while (!plik.eof() == true) {
+		plik >> haslo;
+		if (haslo.length() >= 8) {
+			bool unga=false;
+			for (int i = 0; i <= haslo.length(); i++) {
+				char a = haslo[i];
+				if (a =='$' || a == '*' || a == '#') {
+					unga = true;
+				}
+			}
+
+			if (unga == true) {
+				cout << "zyje"<<endl;
+				has << haslo << endl;
+				unga = false;
+			}
+				
+
+		}
+
+	}
+
+
+}
